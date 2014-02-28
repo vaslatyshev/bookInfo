0d34db5759e630cfe49b765405e6444afe5edc11
Vasiliy <vaslatyshev@gmail.com>
2014-02-28 12:59:33 +0300

    Update GrandTheftRating.php

--- GrandTheftRating.php
+++ GrandTheftRating.php
@@ -7,28 +7,28 @@ Class GrandTheftRating
 	public static function getBookInfo($url)
 	{
 		$userInfo=array();
-		
+
 		if(!($file = @file_get_html($url))){
 	        throw new Exception('Cannot download file ' . $url);
 	    }
-	    
+
 	    $bookInfoResult=new BookInfoResult;
-	    
+
 	    $ratingBar = $file->find('#rating_bar');
 		$ratingBar = reset($ratingBar);
 		$ratingValue=$ratingBar->find('div[itemprop="ratingValue"]');
 		$reviewCount=$ratingBar->find('div[itemprop="reviewCount"]');
-		$allTags=$file->find('#detail_tags div.bMyTag span.bMyTag a');
+		$allTags=$file->find('#detail_tags .make-checkin-form .pseudo-href li span');
 		$ratingValue=reset($ratingValue);
 		$reviewCount=reset($reviewCount);
 		$allTags=reset($allTags);
 		foreach($allTags as $tag) {
-			$bookInfoResult->tags[]=$tag->title;
+			$bookInfoResult->tags[]=$tag->innerText();
 		}
-		
+
 	    $bookInfoResult->ratingValue=($ratingValue?$ratingValue->innerText():0);
 	    $bookInfoResult->reviewCount=($reviewCount?$reviewCount->innerText():0);
-	    
+
 	    return $bookInfoResult;
 	}
 }
